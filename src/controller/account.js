7130fc0882c6ad8349d56356a78684d459e6caf8
Javier Gómez <javiergomezve@gmail.com>
2018-11-22 21:01:34 -0400

    Fixing error response on register account

--- src/controller/account.js
+++ src/controller/account.js
@@ -12,7 +12,12 @@ export default ({config, db}) => {
       username: req.body.email,
     }), req.body.password, (err, account) => {
       if (err) {
-        res.send(err);
+        if (err.name === "UserExistsError") {
+          console.log("User Exists");
+          return res.status(409).send(err);
+        } else {
+          return res.status(500).send(err);
+        }
       }
 
       passport.authenticate('local', {
